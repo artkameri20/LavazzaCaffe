1f888a9b65faed610cfd9a5e2213cc34dd214805
eltion1 <eltionbiba@gmail.com>
2024-05-03 17:44:52 +0200

    i ndreqa

--- js/1.js
+++ js/1.js
@@ -3,68 +3,68 @@
                 id: 0,
                 image: 'images/strawberry.jpeg',
                 title: 'strawberry cake' ,
-                price: 6,
+                
             
             },
             {
                 id: 1,
                 image: 'images/banana.jpeg',
                 title: 'banana cake',
-                price: 6,
+             
             },
             {
                 id: 2,
                 image: 'images/blueberry.jpeg',
                 title: 'blueberry cake',
-                price: 6,
+               
             },
             {
                 id: 3,
                 image: 'images/snickers.jpeg',
                 title: 'snickers cake',
-                price: 6,
+               
             },
             {
                 id: 4,
                 image: 'images/chocolate.jpeg',
                 title: 'chocolate cake',
-                price: 6,
+                
             },
             {
                 id: 5,
                 image: 'images/oreo.jpeg',
                 title: 'oreo cake',
-                price: 6,
+               
             },
              {
                 id: 6,
                 image: 'images/trileqe.jpeg',
                 title: 'trileqe cake',
-                price: 7,
+                
             },
              {
                 id: 7,
                 image: 'images/capucciono.jpeg',
                 title: 'capuccino coffe',
-                price: 4,
+              
             },
              {
                 id: 8,
                 image: 'images/ice.jpeg',
                 title: 'ice coffe',
-                price: 4,
+              
             },
              {
                 id: 9,
                 image: 'images/ecpresso.jpeg',
                 title: 'ecpresso coffe',
-                price: 4,
+               
             },
              {
                 id: 10,
                 image: 'images/maciatoo.jpeg',
                 title: 'maciatoo coffe',
-                price: 4,
+                
             },
         ];
 
@@ -89,9 +89,8 @@
                             <img class='images' src=${image}></img>
                         </div> 
                         <div class='bottom'>
-                            <p>${title}</p>
-                            <h2>$ ${price}.00</h2>
-                        <button>Add to cart</button>
+                           
+                        
                         </div>
                     </div>`
                 )
